e48ea9edfef43c2fd722931e5221226a1f504d76
Le H Ngo <seiji@o2.pl>
2019-05-09 14:21:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+
+function theBeatlesPlay(mus, ins) {
+  var bt = []
+  
+  for (let i = 0; i < mus.length; i++) {
+    bt.push(`${mus[i]} plays ${ins[i]}`)
+  } return bt
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+while (i < facts.length) {
+  
+}
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+while (i < facts.length) {
+  facts[i] = `${facts[i]}` + " !!!"
+  i++
+} return facts
+}
